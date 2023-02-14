dac3fb11150d46fb0996f333eacea1f37199efd3
Monusagar <monusagar9504@gmail.orgg>
2023-02-14 15:09:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const progress = document.querySelector('.progress-done');
+
+progress.style.width = progress.getAttribute('data-done') + '%';
+progress.style.opacity = 1;
+
+const progress1 = document.querySelector('.progress1-done');
+
+progress1.style.width = progress1.getAttribute('data-done') + '%';
+progress1.style.opacity = 1;
+
+const progress2 = document.querySelector('.progress2-done');
+
+progress2.style.width = progress2.getAttribute('data-done') + '%';
+progress2.style.opacity = 1;
